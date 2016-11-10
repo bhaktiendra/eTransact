260e8960dfb5d1d705238f11c0060e9de88248ab
bhaktiendra <bhakti.endra@gmail.com>
2016-11-10 19:40:09 +0800

    initial commit create model

---
+++ PandawaTransact/PandawaTransact/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ETransact;
+
+namespace PandawaTransact
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TransactController controller = new TransactController();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ETransact.Menu menu = new ETransact.Menu();
+
+            menu.Id = 1;
+            menu.Nama = "Test";
+            menu.Harga = 2000;
+            menu.Diskon = 50;
+            menu.Deskripsi = "test desc";
+            menu.Metode = "metode";
+            menu.Kategori = "test";
+
+            controller.AddMenu(menu);
+        }
+    }
+}
